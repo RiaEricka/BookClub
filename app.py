df8cab3653af7a5462e80b0b5f90fe5b0b910866
Ria <156914203+RiaEricka@users.noreply.github.com>
2025-08-07 06:52:30 +0200

    Update app.py
    adding a database

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db' #creates database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#create db model
+class Books(db.Model):
+    book_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100),nullable=False)
+    author = db.Column(db.String(100),nullable=False)
+     
 
 @app.route("/")
 def home():
